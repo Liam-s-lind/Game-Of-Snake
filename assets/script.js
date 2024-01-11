577dc66b625e68c20e25e2ab7f8b2d0420778213
Liam-s-lind <>
2024-01-11 16:13:21 +0000

    global variables and initialization code

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+//Board
+var blockSize = 20;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//the snakes head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//the snakes food
+var foodX;
+var foodY;
+
+var gameOver = false;
+
